e5368caa0eebd109e7835a00725a3cb5dc7d43c0
Boye <samuel.boye95@yahoo.com>
2022-08-19 14:44:38 +0100

    add .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-
+import { BlogPost, Home, Login, Publish, Tags } from './pages'
 
 function App() {
 
   return (
-    <div className="bg-red-400 px-9">
+    <>
       <Home />
-    </div>
+    </>
   )
 }
 
