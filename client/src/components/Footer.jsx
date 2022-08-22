2f02b3a88e881fe6d8ba4d58d3487b6de8906039
Boye <samuel.boye95@yahoo.com>
2022-08-22 15:04:44 +0100

    add footer section 80% done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,45 +1,52 @@
 import React from 'react'
 import Ticker from 'react-ticker'
 
+let Tick = () => {
+  return (
+    <Ticker speed={10}>
+      {({index}) => (
+        <>
+          <div className='font-sfproth text-2xl whitespace-nowrap flex gap-4'>
+            <span>DIGITAL PRODUCT DESIGN</span>
+            <span className='font-sfprotr'>REMOTE WORK</span>
+            <span>UX DESIGN</span>
+            <span className='font-sfprotr'>DISTRIBUTED TEAMS</span>
+            <span>CREATIVITY</span>
+            <span className='font-sfprotr'>STRATEGY</span>
+            <span>SUSPENSE</span>
+            <span className='font-sfprotr'>GROWTH #{index}</span>
+            <span>DIGITAL PRODUCT DESIGN</span>
+            <span className='font-sfprotr'>REMOTE WORK</span>
+            <span>UX DESIGN</span>
+            <span className='font-sfprotr'>DISTRIBUTED TEAMS</span>
+            <span>CREATIVITY</span>
+            <span className='font-sfprotr'>STRATEGY</span>
+            <span>SUSPENSE</span>
+            <span className='font-sfprotr'>GROWTH</span>
+          </div>
+        </>
+      )}
+    </Ticker>
+  )
+}
+
 export default function Footer () {
   return (
     <footer className='w-full bg-black text-white absolute bottom-0 h-96'>
-      <Ticker speed={10} mode={'chain'}>
-        {({ index }) => (
-          <div className='font-sfproth text-2xl whitespace-nowrap flex'>
-            <p>DIGITAL PRODUCT DESIGN</p>
-            <p>REMOTE WORK</p>
-            <p>UX DESIGN</p>
-            <p>DISTRIBUTED TEAMS</p>
-            <p>CREATIVITY</p>
-            <p>STRATEGY</p>
-            <p>SUSPENSE</p>
-            <p>GROWTH</p>
-            <p>DIGITAL PRODUCT DESIGN</p>
-            <p>REMOTE WORK</p>
-            <p>UX DESIGN</p>
-            <p>DISTRIBUTED TEAMS</p>
-            <p>CREATIVITY</p>
-            <p>STRATEGY</p>
-            <p>SUSPENSE</p>
-            <p>GROWTH</p>
-            <p>{index}</p>
-          </div>
-        )}
-      </Ticker>
-      <div className=''>
-        <div className='red'>ÓNYÉ ŃKÚZÍ</div>
-        <p className=''>
+      <Tick />
+      <div className='mt-24 flex flex-col items-center gap-6'>
+        <div className='font-zeit text-3xl'>ÓNYÉ ŃKÚZÍ</div>
+        <p className='w-2/6 text-center font-nysmall text-sm'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit
           tempus erat egestas efficitur. In hac habitasse platea dictumst. Fusce
           a nunc eget ligula suscipit finibus.
         </p>
-        <div className=''>
-          <a href='#'>Twitter</a>
-          <a href='#'>LinkedIn</a>
-          <a href='#'>Github</a>
+        <div className='flex gap-4 font-sfprotr text-sm [&>*]:underline'>
+          <a href='#' className='hover:no-underline'>Twitter</a>
+          <a href='#' className='hover:no-underline'>LinkedIn</a>
+          <a href='#' className='hover:no-underline'>Github</a>
         </div>
-        <div className=''>
+        <div className='font-sfprotr text-sm flex flex-col items-center'>
           <span>© 2022 Adeboye Folaranmi</span>
           <span>All rights reserved.</span>
         </div>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header () {
     <header className='border-b-2'>
       <div className='w-5/6 mx-auto h-20 flex justify-between items-center'>
         <div className='uppercase w-3/5 font-zeit text-4xl'>ónyé ńkúzí</div>
-        <nav className='w-2/5 flex justify-evenly font-nysmall'>
+        <nav className='w-2/5 flex justify-evenly font-nysmall [&>a]:hover:text-gray-500'>
           <a href='#'>Blog</a>
           <a href='#'>About</a>
           <a href='#'>Links</a>
