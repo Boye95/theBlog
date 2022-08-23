888d937c771955583e19fadd91b4ffe27d000205
Boye <samuel.boye95@yahoo.com>
2022-08-23 13:33:57 +0100

    dome with single post

--- client/src/pages/BlogPost.jsx
+++ client/src/pages/BlogPost.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import img1 from '../assets/postimages/1.png'
 import avatar from '../assets/avatar.png'
+import inpostimg from '../assets/inpostimgs/1.png'
 import { FaTwitterSquare, FaFacebookSquare } from 'react-icons/fa'
 
 export default function BlogPost () {
@@ -31,11 +32,107 @@ export default function BlogPost () {
             </div>
           </div>
           <div className='flex w-3/6 justify-end items-center gap-2'>
-            <div className='border-2 px-2 py-1'>
-              <FaFacebookSquare className='w-8 h-6' />
+            <div className='border-2 rounded py-1 w-16'>
+              <FaFacebookSquare className='w-8 h-6 mx-auto' />
             </div>
-            <div className='border-2 px-2 py-1'>
+            <div className='border-2 rounded py-1 w-16'>
+              <FaTwitterSquare className='w-8 h-6 mx-auto' />
+            </div>
+          </div>
+        </div>
+
+        <div id='postcontent' className='mt-16'>
+          <p className='font-nymedium'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu
+            velit tempus erat egestas efficitur. In hac habitasse platea
+            dictumst. Fusce a nunc eget ligula suscipit finibus. Aenean pharetra
+            quis lacus at viverra. lectus.
+          </p>
+          <p className='mt-4 font-nymedium'>
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+            per inceptos himenaeos. Aliquam quis posuere ligula. In eu dui
+            molestie, molestie lectus eu, semper Class aptent taciti sociosqu ad
+            litora torquent per conubia nostra, per inceptos himenaeos. Aliquam
+            quis posuere ligula. In eu dui molestie, molestie lectus eu, semper
+          </p>
+          <h3 className='mt-4 font-sfprod'>Next on the pipeline</h3>
+          <p className='font-nymedium mt-4'>
+            Duis eu velit tempus erat egestas efficitur. In hac habitasse platea
+            dictumst. Fusce a nunc eget ligula suscipit finibus. Aenean pharetra
+            quis lacus at viverra. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per inceptos himenaeos. Morbi efficitur
+            auctor metus, id mollis lorem pellentesque id. Nullam posuere
+            maximus dui et fringilla.
+          </p>
+          <p className='font-nymedium mt-4'>
+            Aenean pharetra quis lacus at viverra. Class aptent taciti sociosqu
+            ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi
+            efficitur auctor metus, id mollis lorem pellentesque id. Nullam
+            posuere maximus dui et fringilla.
+          </p>
+          <div className='mt-6'>
+            <img src={inpostimg} alt='' />
+            <p className='w-3/5 mt-2 mx-auto text-center'>
+              Image caption centered this way and I'll make this a bit longer to
+              indicate the amount of line-height.
+            </p>
+          </div>
+          <p className='font-nymedium mt-4'>
+            Duis eu velit tempus erat egestas efficitur. In hac habitasse platea
+            dictumst. Fusce a nunc eget ligula suscipit finibus. Aenean pharetra
+            quis lacus at viverra. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per inceptos himenaeos. Morbi efficitur
+            auctor metus, id mollis lorem pellentesque id. Nullam posuere
+            maximus dui et fringilla.
+          </p>
+          <p className='font-nymedium mt-4'>
+            Aenean pharetra quis lacus at viverra. Class aptent taciti sociosqu
+            ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi
+            efficitur auctor metus, id mollis lorem pellentesque id. Nullam
+            posuere maximus dui et fringilla.
+          </p>
+
+          <div className='mt-8 font-nymedium'>
+            <p className=''>A list looks like this:</p>
+            <ul className='list-disc ml-4 leading-9'>
+              <li>First item in the list</li>
+              <li>Second item in the list posuere maximus dui et fringilla</li>
+              <li>Third item in the list</li>
+            </ul>
+          </div>
+          <p className='font-nymedium mt-4'>
+            Aenean pharetra quis lacus at viverra. Class aptent taciti sociosqu
+            ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi
+            efficitur auctor metus
+          </p>
+          <div className='font-nymedium mt-4'>
+            <p className=''>Thanks for reading,</p>
+            <p className=''>Boye</p>
+          </div>
+
+          <div className='mt-8 flex w-full items-center gap-2'>
+            <div className='border-2 rounded py-2 w-3/6 flex justify-center font-sfprotr'>
+              <FaFacebookSquare className='w-8 h-6' />
+              <p className=''>Share on Facebook</p>
+            </div>
+            <div className='border-2 rounded py-2 w-3/6 flex justify-center font-sfprotr'>
               <FaTwitterSquare className='w-8 h-6' />
+              <p className=''>Share on Twitter</p>
+            </div>
+          </div>
+
+          <div className='font-sfprotr mt-7'>
+            Tags: product, design, culture
+          </div>
+
+          <div className='mt-9 border-t-2 border-dotted border-black'>
+            <div className='mt-4 flex items-center gap-4'>
+              <img src={avatar} alt='' className='w-16 rounded-3xl' />
+              <p className='font-nymedium'>
+                <span className='font-sfproth'>ADEBOYE FOLARANMI</span> is a Design Founder & Advisor, Berlin School of
+                Creative Leadership Executive MBA participant, Zippie advisor,
+                Wolt co-founder, and Nordic Rose stakeholder.
+              </p>
             </div>
           </div>
         </div>
