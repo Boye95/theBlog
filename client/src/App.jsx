f9dfff53fd8309fe9bf112c039844769ab3a8846
Boye <samuel.boye95@yahoo.com>
2022-09-10 12:46:55 +0100

    added react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
 import { Header, Footer } from './components'
 import { BlogPost, Home, Login, Publish, Tags, Register } from './pages'
 
-
-function App() {
-
+function App () {
   return (
     <>
-      {/* <Header /> */}
-      {/* <Home /> */}
-      {/* <BlogPost />  */}
-      <Login />
-      {/* <Register /> */}
-      {/* <Footer /> */}
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/blogpost' element={<BlogPost />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
