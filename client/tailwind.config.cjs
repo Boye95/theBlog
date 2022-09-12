42325d06b5b148a888e6510ee05b3d10bca779f1
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-09-12 12:41:28 +0100

    custom breakpoints added to tailwind config

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+
+      xs: { max: "400px" },
+      // => @media (max-width: 400px) { ... }
+    },
     extend: {
       fontFamily: {
         nylarge: ["NY-ExtraLarge", "sans-serif"],
@@ -16,10 +33,10 @@ module.exports = {
         zeit: ["Zeit-Light", "sans-serif"],
       },
       backgroundImage: {
-        'moon': "url('/src/assets/moon.jpg')",
-        'sun': "url('/src/assets/sun.jpg')",
-      }
+        moon: "url('/src/assets/moon.jpg')",
+        sun: "url('/src/assets/sun.jpg')",
+      },
     },
   },
   plugins: [],
-}
+};
