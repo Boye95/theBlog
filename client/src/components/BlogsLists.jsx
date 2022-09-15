8adb88fc95f5353058b3b3b382624afb20bd6494
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-09-15 22:08:24 +0100

    responsive styles for article list

--- client/src/components/BlogsLists.jsx
+++ client/src/components/BlogsLists.jsx
@@ -9,7 +9,10 @@ export default function BlogsLists () {
   return (
     <div className='w-3/5 mx-auto mt-9 xl:w-4/5 lg:w-full'>
       <Link to='/blogpost'>
-        <div id='article1' className='flex flex-col items-center border-b-2 pb-11'>
+        <div
+          id='article1'
+          className='flex flex-col items-center border-b-2 pb-11'
+        >
           <img src={img1} alt='mainpost' className='w-4/5 lg:w-full' />
           <h1 className='font-nylarge w-4/5 text-4xl text-center mt-7 md:text-xl'>
             A few words about this blog platform, Ghost, and how this site was
@@ -26,44 +29,20 @@ export default function BlogsLists () {
 
         <div
           id='bloglists'
-          className='mt-7 grid grid-cols-2 justify-center gap-6 gap-y-8 mb-20 md:grid-cols-1 md:justify-center md:bg-blue-400'
+          className='mt-7 grid grid-cols-2 justify-center gap-6 gap-y-8 mb-20 md:grid-cols-1 md:justify-center'
         >
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
-          <Link to='/blogpost'>
-            <Post />
-          </Link>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
         </div>
       </div>
     </div>
--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import img2 from '../assets/postimages/2.png'
 
 export default function Post () {
   return (
     <div>
-      <div id='blog' className='flex flex-col justify-center items-center bg-red-200'>
-        <img src={img2} alt='' className='w-80' />
-        <p className='font-sfprod mt-4 text-center'>
-          Here are some things you should know regarding how we work
-        </p>
+      <div
+        id='blog'
+        className='flex flex-col justify-center items-center sm:w-4/5 md:w-3/5 md:mx-auto'
+      >
+        <Link to='/blogpost'>
+          <img src={img2} alt='' className='w-80' />
+        </Link>
+        <Link to='/blogpost'>
+          <p className='font-sfprod mt-4 text-center hover:underline'>
+            Here are some things you should know regarding how we work
+          </p>
+        </Link>
       </div>
     </div>
   )
