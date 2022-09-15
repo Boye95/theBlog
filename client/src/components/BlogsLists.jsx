26185ea1d9309b76654b189caef30c105d298ce3
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-09-15 13:49:36 +0100

    styling for tabs and mobile

--- client/src/components/BlogsLists.jsx
+++ client/src/components/BlogsLists.jsx
@@ -7,26 +7,26 @@ import Post from './Post'
 
 export default function BlogsLists () {
   return (
-    <div className='w-3/5 mx-auto mt-9'>
+    <div className='w-3/5 mx-auto mt-9 xl:w-4/5 lg:w-full'>
       <Link to='/blogpost'>
-        <div id='article1' className='flex flex-col items-center'>
-          <img src={img1} alt='mainpost' className='w-4/5' />
-          <h1 className='font-nylarge w-4/5 text-4xl text-center mt-7'>
+        <div id='article1' className='flex flex-col items-center border-b-2 pb-11'>
+          <img src={img1} alt='mainpost' className='w-4/5 lg:w-full' />
+          <h1 className='font-nylarge w-4/5 text-4xl text-center mt-7 md:text-xl'>
             A few words about this blog platform, Ghost, and how this site was
             made
           </h1>
-          <h3 className='font-sfmono text-lg w-3/5 text-center mt-8'>
+          <h3 className='font-sfmono text-lg w-3/5 text-center mt-8 md:text-base md:w-4/5'>
             Why Ghost (& Figma) instead of Medium, WordPress or other options?
           </h3>
         </div>
       </Link>
 
-      <div id='all__articles' className='w-3/5 border-t-2 mt-7 mx-auto'>
+      <div id='all__articles' className='w-3/5 mt-7 mx-auto md:w-full'>
         <h3 className='mt-8 text-center font-nylarge text-2xl'>All Articles</h3>
 
         <div
           id='bloglists'
-          className='mt-7 grid grid-cols-2 gap-6 gap-y-8 mb-20'
+          className='mt-7 grid grid-cols-2 justify-center gap-6 gap-y-8 mb-20 md:grid-cols-1 md:justify-center md:bg-blue-400'
         >
           <Link to='/blogpost'>
             <Post />
--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -4,9 +4,9 @@ import img2 from '../assets/postimages/2.png'
 export default function Post () {
   return (
     <div>
-      <div id='blog' className='text-center'>
-        <img src={img2} alt='' />
-        <p className='font-sfprod mt-4 mx-auto w-11/12'>
+      <div id='blog' className='flex flex-col justify-center items-center bg-red-200'>
+        <img src={img2} alt='' className='w-80' />
+        <p className='font-sfprod mt-4 text-center'>
           Here are some things you should know regarding how we work
         </p>
       </div>
