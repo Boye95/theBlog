1709d6f1c953d68b5b55784555fc2fb9ca6ac14d
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-09-15 22:53:42 +0100

    added active bottom border to navlinks

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,58 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function Header () {
+  // This styling will be applied to a <NavLink> when the
+  // route that it links to is currently selected.
+  let activeStyle = {
+    borderBottom: '2px solid black'
+  }
+
+  let activeClassName = '2px solid black'
+
   return (
     <header className='border-b-2'>
       <div className='w-5/6 mx-auto h-20 flex justify-between items-center'>
-        <Link to='/' className='uppercase w-3/5 font-zeit text-4xl'>ónyé ńkúzí</Link>
-        <nav className='w-2/5 flex justify-evenly font-nysmall'>
-          <Link to='/' className='hover:text-gray-400'>Blog</Link>
-          <Link to='/about' className='hover:text-gray-400'>About</Link>
-          <Link to='/login' className='hover:text-gray-400'>Login</Link>
-          <Link to='/register' className='hover:text-gray-400'>Register</Link>
-          {/* <Link to='/project' className='hover:text-gray-400'>Projects</Link> */}
-          <Link to='/publish' className='hover:text-gray-400'>Publish</Link>
+        <NavLink to='/' className='uppercase w-3/5 font-zeit text-4xl'>
+          ónyé ńkúzí
+        </NavLink>
+        <nav className='w-2/5 h-full flex justify-between items-center font-nysmall'>
+          <NavLink
+            to='/'
+            className='navlink'
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Blog
+          </NavLink>
+          <NavLink
+            to='/about'
+            className='navlink'
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            About
+          </NavLink>
+          <NavLink
+            to='/login'
+            className='navlink'
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Login
+          </NavLink>
+          <NavLink
+            to='/register'
+            className='navlink'
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Register
+          </NavLink>
+          {/* <NavLink to='/project' className='navlink'>Projects</NavLink> */}
+          <NavLink
+            to='/publish'
+            className='navlink'
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Publish
+          </NavLink>
         </nav>
       </div>
     </header>
