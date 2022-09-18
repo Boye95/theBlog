f3c0268f89c355970533fb120170bfea55cd01ea
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-09-18 21:16:13 +0100

    made footer responsiveness

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -44,8 +44,8 @@ export default function Footer () {
     <footer className='w-full bg-black text-white h-auto pb-4'>
       <Tick />
       <div className='mt-24 flex flex-col items-center gap-6'>
-        <div className='font-zeit text-3xl'>ÓNYÉ ŃKÚZÍ</div>
-        <p className='w-2/6 text-center font-nysmall text-sm'>
+        <div className='font-zeit text-3xl sm:text-2xl'>ÓNYÉ ŃKÚZÍ</div>
+        <p className='w-2/6 text-center font-nysmall text-sm ham:w-4/6'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit
           tempus erat egestas efficitur. In hac habitasse platea dictumst. Fusce
           a nunc eget ligula suscipit finibus.
