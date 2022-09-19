8da28f46ccbd1b3e0502d390fef13125324895c3
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-09-19 22:55:47 +0100

    imported react router's Outlet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,29 @@
 import { Routes, Route } from 'react-router-dom'
-import { Header, Footer } from './components'
-import { BlogPost, Home, Login, Profile, Publish, Tags, Register } from './pages'
+import { Header, Layout } from './components'
+import {
+  BlogPost,
+  Home,
+  Login,
+  Profile,
+  Publish,
+  Tags,
+  Register
+} from './pages'
 
 function App () {
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/blogpost' element={<BlogPost />} />
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/blogpost' element={<BlogPost />} />
+        </Route>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/publish' element={<Publish />} />
-        <Route path="/profile" element={<Profile />} />
-        
+        <Route path='/profile' element={<Profile />} />
       </Routes>
-      <Footer />
     </>
   )
 }
