e6112bc5311d2e9968c9308f1f2f8e3dff5b19bc
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-09-29 17:18:55 +0100

    adding and styling other input components

--- client/src/pages/Publish/LexicalFiles/LexicalEditor.jsx
+++ client/src/pages/Publish/LexicalFiles/LexicalEditor.jsx
@@ -21,35 +21,66 @@ import {
 
 const NoteViewer = () => {
   return (
-    <EditorComposer>
-      <Editor hashtagsEnabled={true} emojisEnabled={true}>
-        <ToolbarPlugin defaultFontSize='20px' className='toolbar'>
-          <FontFamilyDropdown />
-          <FontSizeDropdown />
-          <Divider />
-          <BoldButton />
-          <ItalicButton />
-          <UnderlineButton />
-          <CodeFormatButton />
-          <InsertLinkButton />
-          <TextColorPicker />
-          <BackgroundColorPicker />
-          <TextFormatDropdown />
-          <Divider />
-          <InsertDropdown
-            enablePoll={true}
-            enableTwitter={true}
-            enableYoutube={true}
-            enableExcalidraw={true}
-            enableHorizontalRule={true}
-            enableEquations={true}
-            enableStickyNote={true}
-          />
-          <Divider />
-          <AlignDropdown />
-        </ToolbarPlugin>
-      </Editor>
-    </EditorComposer>
+    <form className=''>
+      <div className=''>
+        <div className=''>
+          <label htmlFor='blogimg'>
+            <input type='file' name='blogimg' id='blogimg' required />
+          </label>
+          <div className=''>
+            <input
+              type='text'
+              name='blogTitle'
+              id='blogTitle'
+              placeholder='Title...'
+              className=''
+              required
+            />
+            <input
+              type='text'
+              name='blogSubtitle'
+              id='blogSubtitle'
+              placeholder='Subtitle...'
+              className=''
+              required
+            />
+          </div>
+        </div>
+      </div>
+      <EditorComposer>
+        <Editor
+          hashtagsEnabled={true}
+          emojisEnabled={true}
+          placeholder='Say your piece...'
+        >
+          <ToolbarPlugin defaultFontSize='20px' className='toolbar'>
+            <FontFamilyDropdown />
+            <FontSizeDropdown />
+            <Divider />
+            <BoldButton />
+            <ItalicButton />
+            <UnderlineButton />
+            <CodeFormatButton />
+            <InsertLinkButton />
+            <TextColorPicker />
+            <BackgroundColorPicker />
+            <TextFormatDropdown />
+            <Divider />
+            <InsertDropdown
+              enablePoll={true}
+              enableTwitter={true}
+              enableYoutube={true}
+              enableExcalidraw={true}
+              enableHorizontalRule={true}
+              enableEquations={true}
+              enableStickyNote={true}
+            />
+            <Divider />
+            <AlignDropdown />
+          </ToolbarPlugin>
+        </Editor>
+      </EditorComposer>
+    </form>
   )
 }
 
