317b66814e00bc7a45729f2073dcc9918ce4e83c
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-09-30 16:10:34 +0100

    styling lexical page component

--- client/src/pages/Publish/LexicalFiles/LexicalEditor.jsx
+++ client/src/pages/Publish/LexicalFiles/LexicalEditor.jsx
@@ -21,18 +21,18 @@ import {
 
 const NoteViewer = () => {
   return (
-    <form className=''>
-      <div className='flex flex-col items-center '>
-        <label htmlFor='blogimg'>
+    <form className='w-[80%] mx-auto'>
+      <div className='flex flex-col items-center w-full mx-auto'>
+        <label htmlFor='blogimg' className='mb-4'>
           <input type='file' name='blogimg' id='blogimg' required />
         </label>
-        <div className=''>
+        <div className='flex flex-col w-full gap-2 [&>*]:h-[3rem] [&>*]:outline-none [&>*]:rounded'>
           <input
             type='text'
             name='blogTitle'
             id='blogTitle'
             placeholder='Title...'
-            className=''
+            className='w-full max-w-[1050px] mx-auto ring-gray-700 ring-offset-2 ring-2 border-2 text-3xl px-3 font-nylarge transition focus:border-emerald-300 focus:ring-emerald-300 focus:shadow-emerald-300 focus:shadow-[0_0_15px]'
             required
           />
           <input
@@ -40,12 +40,12 @@ const NoteViewer = () => {
             name='blogSubtitle'
             id='blogSubtitle'
             placeholder='Subtitle...'
-            className=''
+            className='ring-gray-700 ring-offset-2 ring-2 border-2 w-[98%] max-w-[1000px] mx-auto mt-2 text-2xl px-3 font-sfmono transition focus:border-emerald-300 focus:ring-emerald-300 focus:shadow-emerald-300 focus:shadow-[0_0_15px]'
             required
           />
         </div>
       </div>
-      <EditorComposer className='editor-shell bg-red-500'>
+      <EditorComposer className='editor-shell'>
         <Editor
           hashtagsEnabled={true}
           emojisEnabled={true}
