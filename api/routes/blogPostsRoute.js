b14511ba65499bfa13af6aa7d543f96801700a2d
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-11-13 22:47:51 +0100

    tested db connected and saved dummy data

--- api/routes/blogPostsRoute.js
+++ api/routes/blogPostsRoute.js
@@ -1,27 +1,33 @@
-const express = require('express')
+const express = require("express");
+const BlogPost = require("../models/blogPostsModel");
 
-const router =express.Router()
-
-
+const router = express.Router();
 
 // Get all blog posts
-router.get('/', (req, res) => {
-    res.json({ message: 'This is all the blog posts' })
-})
-
-// Get single blog post
-router.get('/:id', (req, res) => {
-    res.json({ message: "this is a single blog post" })
-})
-
-// Create a new blog post
-router.post('/', (req, res) => {
-    res.json({ message: 'This is a new blog post' })
-})
-
-// Update a blog post
-router.patch('/:id', (req, res) => {
-    res.json({ message: 'This is a blog post update' })
+router.get("/", (req, res) => {
+  res.json({ message: "This is all the blog posts" });
 });
 
-module.exports = router
+// Get single blog post
+router.get("/:id", (req, res) => {
+  res.json({ message: "this is a single blog post" });
+});
+
+// Create a new blog post
+router.post("/", (req, res) => {
+  const { title, subtitle, body, displayImage, tags } = req.body;
+
+  try {
+    const post = BlogPost.create({ title, subtitle, body, displayImage, tags });
+  } catch (error) {
+    console.log(error);
+  }
+  res.json({ message: "This is a new blog post" });
+});
+
+// Update a blog post
+router.patch("/:id", (req, res) => {
+  res.json({ message: "This is a blog post update" });
+});
+
+module.exports = router;
