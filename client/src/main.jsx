20f15aefc41d0cf74ae670942626e16b2f995b4c
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-11-14 17:14:34 +0100

    installed axios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,21 +6,24 @@ import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
 
 // IMPORT ReactQuery
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain='dev-e0q82q5v.us.auth0.com'
-      clientId='Nc8k52VB8H1NbjriNR41sjmQoaywzEAb'
-      redirectUri={window.location.origin}
-    >
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Auth0Provider>
+    <QueryClientProvider client={queryClient}>
+      <Auth0Provider
+        domain='dev-e0q82q5v.us.auth0.com'
+        clientId='Nc8k52VB8H1NbjriNR41sjmQoaywzEAb'
+        redirectUri={window.location.origin}
+      >
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Auth0Provider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 )
