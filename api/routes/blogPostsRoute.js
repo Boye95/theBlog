37bc6d8e763c8bdc6eb6fa1aff72304ccf1794ce
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-11-18 23:16:28 +0100

    Bad Request

--- api/routes/blogPostsRoute.js
+++ api/routes/blogPostsRoute.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const multer = require("multer");
-const cloudinary = require("cloudinary").v2;
+
 
 const {
   getAllBlogPosts,
@@ -8,46 +7,10 @@ const {
   createBlogPost,
   deleteBlogPost,
   updateBlogPost,
+  upload
 } = require("../controllers/blogPostsController");
 
 const router = express.Router();
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "images");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const imageFilter = (req, file, cb) => {
-//   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-//     return cb(new Error("Only image files are allowed!"), false);
-//   }
-//   cb(null, true);
-// };
-
-// const upload = multer({
-//   storage: storage,
-//   fileFilter: imageFilter,
-// });
-
-// cloudinary.config({
-//   secure: true
-// });
-
-// router.post("/api/upload", upload.single("image"), (req, res) => {
-//   cloudinary.uploader.upload(req.file.path, (err, result) => {
-//     if (err) {
-//       return res.status(400).json({
-//         error: err,
-//       });
-//     }
-//     res.json(result);
-//   })
-//   res.status(200).json("File uploaded");
-// });
 
 // Get all blog posts
 router.get("/", getAllBlogPosts);
@@ -56,7 +19,7 @@ router.get("/", getAllBlogPosts);
 router.get("/:id", getSingleBlogPost);
 
 // Create a new blog post
-router.post("/", createBlogPost);
+router.post("/", upload.single("displayImage"), createBlogPost);
 
 // Delete a blog post
 router.delete("/:id", deleteBlogPost);
