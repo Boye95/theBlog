30a0341b7d936fee460a9b6fc1cbf79f99ec06ee
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-11-20 23:20:59 +0100

    working on other post display

--- client/src/components/OtherArticles.jsx
+++ client/src/components/OtherArticles.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import Post from './Post'
 
-export default function OtherArticles() {
+export default function OtherArticles({post, path}) {
+  console.log(post)
   return (
     <div className='mt-8 grid grid-cols-3 gap-6 gap-y-8 mb-20 lg:grid-cols-2 sm:grid-cols-1'>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+      
     </div>
   )
 }
--- client/src/pages/BlogPost.jsx
+++ client/src/pages/BlogPost.jsx
@@ -1,26 +1,53 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import moment from 'moment'
+import {$generateHtmlFromNodes} from '@lexical/html';
 import eyes from '../assets/eyes.svg'
 import img1 from '../assets/postimages/1.png'
 import avatar from '../assets/avatar.png'
 import inpostimg from '../assets/inpostimgs/1.png'
 import { FaTwitterSquare, FaFacebookSquare } from 'react-icons/fa'
 import { BiEdit } from 'react-icons/bi'
-import OtherArticles from '../components/OtherArticles'
-import { Newsletter } from '../components'
+import { OtherArticles, Newsletter } from '../components'
 
 export default function BlogPost () {
+  const location = useLocation()
+  const path = location.pathname.split('/')[2]
+  console.log(path)
+
+  const fetchSinglePost = async () => {
+    const res = await fetch(`http://localhost:4000/api/blogposts/${path}`)
+    return res.json()
+  }
+
+  const { data, isLoading, error } = useQuery(['singlePost'], fetchSinglePost)
+  const post = data?.data?.post
+  console.log(post)
+
+  // const postBody = post?.body
+  // let handleBody = (state, postBody) => {
+  //   postBody.read(() => {
+  //     const htmlString = $generateHtmlFromNodes(postBody, null)
+  //     console.log(htmlString)
+  //   })
+  // }
+  // handleBody()
+
   return (
     <div className=''>
       <div id='article1' className='flex flex-col items-center'>
         <h1 className='font-nylarge w-3/6 text-4xl text-center mt-7 ham:w-5/6 sm:w-11/12 sm:text-3xl '>
-          A few words about this blog platform, Ghost, and how this site was
-          made
+          {post?.title}
         </h1>
         <h3 className='font-sfmono text-lg w-2/5 text-center mt-8 ham:w-3/5 sm:w-11/12 sm:text-sm'>
-          Why Ghost (& Figma) instead of Medium, WordPress or other options?
+          {post?.subtitle}
         </h3>
-        <img src={img1} alt='mainpost' className='mt-8' />
+        <img
+          src={post?.displayImage.url}
+          alt='post image'
+          className='mt-8 w-3/6 ham:w-11/12 xl:w-4/6'
+        />
       </div>
 
       <div className='mt-8 w-3/6 border-t-2 border-black mx-auto ham:w-11/12 xl:w-4/6'>
@@ -30,9 +57,11 @@ export default function BlogPost () {
               <img src={avatar} alt='' className='w-full' />
             </div>
             <div className=''>
-              <Link to="/profile" className='font-sfproth'>ADEBOYE FOLARANMI</Link>
-              <div className='font-sfprotr'>
-                <span>Apr 15, 2022 </span>
+              <Link to='/profile' className='font-sfproth'>
+                ADEBOYE FOLARANMI
+              </Link>
+              <div className='font-sfprotr flex gap-2 xl:text-sm'>
+                <span>{moment(post?.createdAt).format('lll')}</span>
                 <span>· 4 min read</span>
               </div>
             </div>
@@ -51,74 +80,10 @@ export default function BlogPost () {
         </div>
 
         <div id='postcontent' className='mt-16'>
-          <p className='font-nymedium'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu
-            velit tempus erat egestas efficitur. In hac habitasse platea
-            dictumst. Fusce a nunc eget ligula suscipit finibus. Aenean pharetra
-            quis lacus at viverra. lectus.
-          </p>
-          <p className='mt-4 font-nymedium'>
-            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-            per inceptos himenaeos. Aliquam quis posuere ligula. In eu dui
-            molestie, molestie lectus eu, semper Class aptent taciti sociosqu ad
-            litora torquent per conubia nostra, per inceptos himenaeos. Aliquam
-            quis posuere ligula. In eu dui molestie, molestie lectus eu, semper
-          </p>
-          <h3 className='mt-4 font-sfprod'>Next on the pipeline</h3>
-          <p className='font-nymedium mt-4'>
-            Duis eu velit tempus erat egestas efficitur. In hac habitasse platea
-            dictumst. Fusce a nunc eget ligula suscipit finibus. Aenean pharetra
-            quis lacus at viverra. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Morbi efficitur
-            auctor metus, id mollis lorem pellentesque id. Nullam posuere
-            maximus dui et fringilla.
-          </p>
-          <p className='font-nymedium mt-4'>
-            Aenean pharetra quis lacus at viverra. Class aptent taciti sociosqu
-            ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi
-            efficitur auctor metus, id mollis lorem pellentesque id. Nullam
-            posuere maximus dui et fringilla.
-          </p>
-          <div className='mt-6'>
-            <img src={inpostimg} alt='' />
-            <p className='w-3/5 mt-2 mx-auto text-center'>
-              Image caption centered this way and I'll make this a bit longer to
-              indicate the amount of line-height.
-            </p>
-          </div>
-          <p className='font-nymedium mt-4'>
-            Duis eu velit tempus erat egestas efficitur. In hac habitasse platea
-            dictumst. Fusce a nunc eget ligula suscipit finibus. Aenean pharetra
-            quis lacus at viverra. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Morbi efficitur
-            auctor metus, id mollis lorem pellentesque id. Nullam posuere
-            maximus dui et fringilla.
-          </p>
-          <p className='font-nymedium mt-4'>
-            Aenean pharetra quis lacus at viverra. Class aptent taciti sociosqu
-            ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi
-            efficitur auctor metus, id mollis lorem pellentesque id. Nullam
-            posuere maximus dui et fringilla.
-          </p>
+          
+        </div>
 
-          <div className='mt-8 font-nymedium'>
-            <p className=''>A list looks like this:</p>
-            <ul className='list-disc ml-4 leading-9'>
-              <li>First item in the list</li>
-              <li>Second item in the list posuere maximus dui et fringilla</li>
-              <li>Third item in the list</li>
-            </ul>
-          </div>
-          <p className='font-nymedium mt-4'>
-            Aenean pharetra quis lacus at viverra. Class aptent taciti sociosqu
-            ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi
-            efficitur auctor metus
-          </p>
-          <div className='font-nymedium mt-4'>
-            <p className=''>Thanks for reading,</p>
-            <p className=''>Boye</p>
-          </div>
-
+        <div className=''>
           <div className='mt-8 flex w-full items-center gap-2'>
             <div className='border-2 rounded py-2 w-3/6 flex justify-center items-center font-sfprotr'>
               <FaFacebookSquare className='w-8 h-6' />
@@ -156,9 +121,11 @@ export default function BlogPost () {
       </div>
 
       <div className='mt-16 w-3/5 mx-auto ham:w-4/5'>
-        <h2 className='font-nylarge text-4xl text-center lg:text-3xl sm:text-2xl'>What to read next</h2>
+        <h2 className='font-nylarge text-4xl text-center lg:text-3xl sm:text-2xl'>
+          What to read next
+        </h2>
 
-        <OtherArticles />
+        <OtherArticles post={post} path={path} />
       </div>
 
       <div className='mt-16 w-2/5 mx-auto mb-20 xl:w-3/5 sm:w-11/12 xs:w-full'>
