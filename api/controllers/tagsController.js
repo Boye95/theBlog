6471d15f3b68eacf73454a5047dca82cce6f91f6
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-11-27 22:04:55 +0100

    working on tags api

--- api/controllers/tagsController.js
+++ api/controllers/tagsController.js
@@ -19,3 +19,41 @@ exports.createTags = async (req, res) => {
         });
     }
 }
+
+// get tags
+exports.getTags = async (req, res) => {
+    try {
+        const tags = await Tags.find();
+        res.status(200).json({
+            status: 'success',
+            results: tags.length,
+            data: {
+                tags,
+            },
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: error,
+        });
+    }
+}
+
+// update tags
+exports.updateTags = async (req, res) => {
+    try {
+        const tag = await Tags.findByIdAndUpdate(req.params.id);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tag,
+            },
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: error,
+        });
+    }
+    
+}
--- api/models/tagsModel.js
+++ api/models/tagsModel.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const TagsSchema = new Schema(
   {
     name: {
-      type: String,
+      type: Array,
       required: true,
     },
   },
