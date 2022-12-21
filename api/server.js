f308020606d14db834ef0ba4db40e1dca1ccd0a3
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-12-21 21:36:48 +0100

    add cors origins

--- api/server.js
+++ api/server.js
@@ -11,7 +11,9 @@ const authRoute = require("./routes/authRoute");
 const userRoute = require("./routes/userRoute");
 
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://theblogx.onrender.com"],
+}));
 
 
 // API routes
